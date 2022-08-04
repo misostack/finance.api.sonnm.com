173b5ca37b80ad4092116d1cfeb0dae970befa51
misostack <misostack.com@gmail.com>
2022-08-04 13:45:34 +0700

    add example with stream

--- src/example/controllers/examples.controller.ts
+++ src/example/controllers/examples.controller.ts
@@ -1,7 +1,23 @@
 import { Example } from './../interfaces/example.interface';
 import { ExampleService } from './../services/example.service';
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Headers,
+  Ip,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { Body } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { Transform } from 'stream';
+
+import { createWriteStream } from 'fs';
 
 @Controller('examples')
 export class ExamplesController {
@@ -31,5 +47,75 @@ export class ExamplesController {
   @Delete(':id')
   remove() {
     return {};
+  }
+
+  @Post('request-object/express/upload-file')
+  exampleRequestObjectExpressUploadFile(
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const writeStream = createWriteStream('./tmp/example-write-stream.pdf');
+    req.pipe(writeStream);
+    const myMultipartParser = new Transform({
+      transform(buffer, _, done) {
+        console.log(buffer);
+        console.log('-'.repeat(50));
+        done();
+      },
+    });
+    req.pipe(myMultipartParser);
+    req
+      .on('abort', () => res.send({ aborted: 1001 }))
+      .on('err', (err) => res.send(err))
+      .on('data', (buffer) => {
+        console.log('buffer', buffer);
+      })
+      .on('end', () => {
+        console.log('end');
+        res.status(200).json({
+          body: req.body,
+          headers: req.headers,
+        });
+      });
+  }
+
+  @Post('request-object/express/:email')
+  exampleRequestObjectExpress(@Req() req: Request, @Res() res: Response) {
+    const responseData = {
+      approach: 'express',
+      routeParams: req.params,
+      queryParams: req.query,
+      body: req.body,
+      headers: req.headers,
+      ip: req.ip,
+      ips: req.ips,
+      hostname: req.hostname,
+      subdomain: req.subdomains,
+    };
+    res.status(200).json(responseData);
+  }
+
+  @Post('request-object/standard/:email')
+  exampleRequestObjectStandard(
+    @Body() body,
+    @Query() query,
+    @Param('email') email: string,
+    @Headers() headers,
+    @Ip() ip,
+  ) {
+    const responseData = {
+      approach: 'standard',
+      routeParams: { email },
+      queryParams: query,
+      body: body,
+      headers: headers,
+      ip,
+    };
+    return responseData;
+  }
+
+  @Post('built-in-pipes/example-validation-pipe')
+  exampleValidationPipe(@Body() body) {
+    return { body };
   }
 }
